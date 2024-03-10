cc6cc61ce3e9952d58c0f474dd7a2b7458c82a75
Jeremy Soto Flores <108308182+JXSoto26@users.noreply.github.com>
2024-03-10 13:37:04 -0300

    Updated data.js
    Added 3 more tours.

--- src/data.js
+++ src/data.js
@@ -1,38 +1,40 @@
-import tour1 from './images/tour-1.jpeg';
-import tour2 from './images/tour-2.jpeg';
-import tour3 from './images/tour-3.jpeg';
-import tour4 from './images/tour-4.jpeg';
-
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
 export const pageLinks = [
-  { id: 1, href: '#home', text: 'home' },
-  { id: 2, href: '#about', text: 'about' },
-  { id: 3, href: '#services', text: 'services' },
-  { id: 4, href: '#tours', text: 'tours' },
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
 ];
+
 export const socialLinks = [
-  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
-  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
-  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
 ];
 
 export const services = [
   {
     id: 1,
-    icon: 'fas fa-wallet fa-fw',
-    title: 'saving money',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     id: 2,
-    icon: 'fas fa-tree fa-fw',
-    title: 'endless hiking',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
   {
     id: 3,
-    icon: 'fas fa-socks fa-fw',
-    title: 'amazing comfort',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.',
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 ];
 
@@ -40,41 +42,62 @@ export const tours = [
   {
     id: 1,
     image: tour1,
-    date: 'august 26th, 2020',
-    title: 'Tibet Adventure',
-    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
-    location: 'china',
-    duration: 6,
-    cost: 2100,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Cumque",
+    location: "china",
+    duration: "6",
+    cost: "2100",
   },
   {
     id: 2,
     image: tour2,
-    date: 'october 1th, 2020',
-    title: 'best of java',
-    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
-    location: 'indonesia',
-    duration: 11,
-    cost: 1400,
+    date: "sept 26th, 2020",
+    title: "Best Of Java",
+    info: "lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    location: "Indonesia",
+    duration: "11",
+    cost: "1400",
   },
   {
     id: 3,
     image: tour3,
-    date: 'september 15th, 2020',
-    title: 'explore hong kong',
-    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
-    location: 'hong kong',
-    duration: 8,
-    cost: 5000,
+    date: "may 21st, 2020",
+    title: "Explore Hong Kong",
+    info: "lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    location: "Hong Kong",
+    duration: "6",
+    cost: "2100",
   },
   {
     id: 4,
     image: tour4,
-    date: 'december 5th, 2019',
-    title: 'kenya highlights',
-    info: ` Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.`,
-    location: 'kenya',
-    duration: 20,
-    cost: 3300,
+    date: "jan 3rd, 2020",
+    title: "Kenya Highlights",
+    info: "lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    location: "Kenya",
+    duration: "20",
+    cost: "3300",
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "august 1st, 2020",
+    title: "Safari",
+    info: "lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    location: "Africa Savannas",
+    duration: "4",
+    cost: "5000",
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "december 5th, 2020",
+    title: "India Highlights",
+    info: "lorem ipsum dolor Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    location: "India",
+    duration: "18",
+    cost: "10300",
   },
 ];
+
