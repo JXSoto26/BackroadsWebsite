4a68d1b42ce58831139ff96791fc0578b985990b
Jeremy Soto Flores <108308182+JXSoto26@users.noreply.github.com>
2024-03-12 07:42:01 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,6 @@ const Navbar = () => {
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} className='nav-logo' alt='backroads' />
-          <button type='button' className='nav-toggle' id='nav-toggle'>
-            <i className='fas fa-bars'></i>
-          </button>
         </div>
         {/* <!-- left this comment on purpose --> */}
 
